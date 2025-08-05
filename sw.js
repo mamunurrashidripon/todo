b885430d22356cae6d2a97ada1780a4bb92506ba
mamunurrashidripon <mamun.rasid@gmail.com>
2025-08-06 03:28:06 +0600

    local storage functionality added

--- sw.js
+++ sw.js
@@ -1,10 +1,18 @@
 self.addEventListener('install', e => {
+  console.log('Service Worker installed');
+  // Precache minimal app shell if desired (optional)
   e.waitUntil(
-    caches.open('todo-v1').then(cache =>
-      cache.addAll(['/', '/todo/', 'index.html', 'sw.js', 'manifest.json'])
-    )
+    caches.open('todo-v1').then(cache => {
+      return cache.addAll([
+        '/todo/',
+        '/todo/index.html',
+        '/todo/manifest.json',
+        '/todo/sw.js'
+      ]);
+    })
   );
 });
+
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
