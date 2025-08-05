bb1af1d9d539b96834fad975e6a191c210449eaa
mamunurrashidripon <mamun.rasid@gmail.com>
2025-08-06 02:49:44 +0600

    code edited

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
 self.addEventListener('install', e => {
-  console.log('Service Worker installed');
+  e.waitUntil(
+    caches.open('todo-v1').then(cache =>
+      cache.addAll(['/', '/todo/', 'index.html', 'sw.js', 'manifest.json'])
+    )
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
